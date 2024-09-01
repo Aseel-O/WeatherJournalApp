d1a745c21188f70f56c3054906e229e10bd96c81
Aseel Omar <aseel.o4869@gmail.com>
2024-09-01 03:05:12 +0300

    finished project

--- project/app.js
+++ project/app.js
@@ -1,5 +1,98 @@
 /* Global Variables */
-
+let baseURL = `https://api.openweathermap.org/data/2.5/weather?zip=`;
+let apiKey =
+  "&appid=afcc183a26f96dfccf162aceddb1be0d&units=imperial";
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate =
+  d.getMonth() +
+  "." +
+  d.getDate() +
+  "." +
+  d.getFullYear();
+// Event listener to add function to existing HTML DOM element
+document
+  .getElementById("generate")
+  .addEventListener("click", getWeather);
+/* Function called by event listener */
+function getWeather(e) {
+  const zipCode =
+    document.getElementById("zip").value;
+  const feelings =
+    document.getElementById("feelings").value;
+  const user_response = {
+    zip_code: zipCode,
+    feelings: feelings,
+  };
+  getWeatherData(baseURL, zipCode, apiKey)
+    .then(function (data) {
+      postData("/add", {
+        temperature: data.main.temp,
+        date: d,
+        user_response: user_response,
+      });
+    })
+    .then(updateUI);
+}
+
+/* Function to GET Web API Data*/
+const getWeatherData = async (
+  baseURL,
+  zipCode,
+  key
+) => {
+  const res = await fetch(
+    baseURL + zipCode + key
+  );
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+/* Function to POST data */
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+/* Function to GET Project Data */
+const updateUI = async () => {
+  const request = await fetch("/all");
+  try {
+    // Transform into JSON
+    const allData = await request.json();
+    const last = allData.length - 1;
+    document.getElementById("date").innerHTML =
+      allData[last].date;
+    document.getElementById("temp").innerHTML =
+      Math.round(allData[last].temperature) +
+      "degrees";
+    document.getElementById("z").innerHTML =
+      allData[last].user_response.zip_code;
+    document.getElementById("feel").innerHTML =
+      allData[last].user_response.feelings;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
--- server.js
+++ server.js
@@ -1,19 +1,53 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [];
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({ extended: false })
+);
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("project"));
 
 // Setup Server
+const port = 8000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log("server running");
+  console.log(`running on localhost:${port}`);
+}
+
+app.get("/all", getProjectData);
+function getProjectData(req, res) {
+  res.send(projectData);
+  console.log(projectData);
+}
+
+app.post("/add", addProjectData);
+function addProjectData(req, res) {
+  const newEntry = {
+    temperature: req.body.temperature,
+    date: req.body.date,
+    user_response: req.body.user_response,
+  };
+
+  projectData.push(newEntry);
+  res.send(projectData);
+  console.log("data added to project.");
+  console.log(projectData);
+}
